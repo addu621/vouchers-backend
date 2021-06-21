95588a503715e1f7c01d316f97f58ae4894a91aa
Harshit Garg <harshit.garg@hashedin.com>
2021-06-21 17:38:17 +0530

    modified upload file response

--- src/main/java/com/example/server/controllers/UploadImage.java
+++ src/main/java/com/example/server/controllers/UploadImage.java
@@ -7,14 +7,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class UploadImage {
     @Autowired
     private UploadImageService uploadImageService;
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
+    public Map<String,String> uploadFile(@RequestParam("file") MultipartFile file) {
         String url = uploadImageService.uploadFile(file);
-        return url;
+        Map<String, String> res = new HashMap<>();
+
+        res.put("url",url);
+        return res;
     }
 }
