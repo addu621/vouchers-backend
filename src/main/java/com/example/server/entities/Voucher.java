b416f524e8ffda1c29773eca544bc849293f52dc
Harshit Garg <harshit.garg@hashedin.com>
2021-06-21 11:36:16 +0530

    modified voucher entity

--- src/main/java/com/example/server/entities/Voucher.java
+++ src/main/java/com/example/server/entities/Voucher.java
@@ -9,13 +9,15 @@ public class Voucher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long voucher_id;
+    private BigDecimal voucher_value;
     private String voucher_code;
-    private BigDecimal listed_price;
+    private BigDecimal selling_price;
     private String created_on;
     private String expiry_date;
+    private boolean is_negotiable;
     private boolean is_verified;
     private Integer discount;
-    private String website_url;
+    private String description;
 
     @ManyToOne
     @JoinColumn(name="seller_id",referencedColumnName="seller_id", nullable=false)
@@ -33,4 +35,7 @@ public class Voucher {
     @JoinColumn(name="voucher_company", nullable=false)
     private VoucherCompany voucher_company;
 
+    @OneToOne
+    @JoinColumn(name="voucher_category", nullable=false)
+    private VoucherCategory voucher_category;
 }
