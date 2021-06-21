01ccd5cb9ed7af738a9434801a27ecec4cc0c799
Harshit Garg <harshit.garg@hashedin.com>
2021-06-21 16:29:40 +0530

    implemented search-voucher route

--- src/main/java/com/example/server/repositories/PersonRepo.java
+++ src/main/java/com/example/server/repositories/PersonRepo.java
@@ -4,7 +4,9 @@ import com.example.server.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PersonRepo extends JpaRepository<Person,String> {
     public Person findByEmail(String email);
 }
+
