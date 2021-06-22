fbb8844f145e27cd018329d807f85a8d94e1c1c7
Akshat Jain <akshat.jain@hashedin.com>
2021-06-22 16:55:38 +0530

    Added Unverified Vouchers Route

--- src/main/java/com/example/server/repositories/VoucherRepository.java
+++ src/main/java/com/example/server/repositories/VoucherRepository.java
@@ -1,6 +1,7 @@
 package com.example.server.repositories;
 
 import com.example.server.entities.Voucher;
+import com.example.server.enums.VoucherVerificationStatus;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,6 @@ public interface VoucherRepository extends CrudRepository<Voucher,Long> {
     @Query("select v from Voucher v where lower(v.title) like lower(concat('%', :search, '%')) " +
             "or lower(v.description) like lower(concat('%', :search, '%'))")
     List<Voucher> searchVoucher(@Param("search") String search);
+
+    List<Voucher> findByVerificationStatus(VoucherVerificationStatus voucherVerificationStatus);
 }
