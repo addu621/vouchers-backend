abd31f35a6dfa76c53bb11448d0db9750662cb21
Harshit Garg <harshit.garg@hashedin.com>
2021-06-24 16:09:05 +0530

    implemented accept voucher quote price

--- src/main/java/com/example/server/controllers/QuotePriceController.java
+++ src/main/java/com/example/server/controllers/QuotePriceController.java
@@ -3,6 +3,7 @@ package com.example.server.controllers;
 import com.example.server.dto.request.DealRequest;
 import com.example.server.dto.response.GenericResponse;
 import com.example.server.entities.Person;
+import com.example.server.entities.Voucher;
 import com.example.server.entities.VoucherDeal;
 import com.example.server.services.VoucherDealService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 public class QuotePriceController {
@@ -23,11 +25,23 @@ public class QuotePriceController {
         GenericResponse genericResponse = new GenericResponse();
         Person personDetails = (Person) request.getAttribute("person");
         Long buyerId = personDetails.getId();
+        GenericResponse response = voucherDealService.quotePrice(buyerId,dealRequest.getVoucherId(),dealRequest.getQuotedPrice());
 
-        GenericResponse result = voucherDealService.quotePrice(buyerId,dealRequest.getVoucherId(),dealRequest.getQuotedPrice());
-
-        return result;
+        return response;
 
     }
+
+    @PostMapping("/quote/accept")
+    public GenericResponse acceptQuotedPrice(HttpServletRequest request, @RequestBody DealRequest dealRequest){
+        GenericResponse genericResponse = new GenericResponse();
+//        Person personDetails = (Person) request.getAttribute("person");
+//        Long sellerId = personDetails.getId();
+//        List<VoucherDeal> list= (List<VoucherDeal>)
+//                voucherDealService.acceptQuotedPrice(dealRequest.getVoucherId(),dealRequest.getBuyerId());
+//
+        return voucherDealService.acceptQuotedPrice(dealRequest.getVoucherId(), dealRequest.getBuyerId());
+//
+    }
+
 }
 
--- src/main/java/com/example/server/dto/request/DealRequest.java
+++ src/main/java/com/example/server/dto/request/DealRequest.java
@@ -7,5 +7,6 @@ import java.math.BigDecimal;
 @Data
 public class DealRequest {
     private Long voucherId;
+    private Long buyerId;
     private BigDecimal quotedPrice;
 }
