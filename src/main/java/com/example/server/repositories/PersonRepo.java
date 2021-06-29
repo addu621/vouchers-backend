36fa088d1287d493e63b663d5482c807037ecaf8
Harshit Garg <harshit.garg@hashedin.com>
2021-06-29 15:16:24 +0530

    implemented get seller rating repository

--- src/main/java/com/example/server/repositories/PersonRepo.java
+++ src/main/java/com/example/server/repositories/PersonRepo.java
@@ -1,12 +1,19 @@
 package com.example.server.repositories;
 
+import com.example.server.dto.response.SellerRatingResponse;
 import com.example.server.entities.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface PersonRepo extends CrudRepository<Person,Long> {
     public Person findByEmail(String email);
+    @Query("Select new com.example.server.dto.response.SellerRatingResponse(r.sellerId,AVG(r.stars)) "+"From SellerRating AS r Group by r.sellerId")
+    List<SellerRatingResponse> ratings();
+
 }
 
