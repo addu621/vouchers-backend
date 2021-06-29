0cc2bcf3cb297b63c445abd2d7acaa475372f607
Harshit Garg <harshit.garg@hashedin.com>
2021-06-29 15:15:15 +0530

    implemented filter repository

--- src/main/java/com/example/server/repositories/VoucherRepository.java
+++ src/main/java/com/example/server/repositories/VoucherRepository.java
@@ -1,6 +1,8 @@
 package com.example.server.repositories;
 
+import com.example.server.dto.response.SellerRatingResponse;
 import com.example.server.dto.response.VoucherResponse;
+import com.example.server.entities.SellerRating;
 import com.example.server.entities.Voucher;
 import com.example.server.enums.VoucherVerificationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +11,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+
 
 @Repository
 public interface VoucherRepository extends JpaRepository<Voucher,Long> {
@@ -18,8 +22,11 @@ public interface VoucherRepository extends JpaRepository<Voucher,Long> {
             "or lower(v.description) like lower(concat('%', :search, '%'))")
     List<Voucher> searchVoucher(@Param("search") String search);
 
-    @Query("select v from Voucher v where v.companyId in (:companies) and v.categoryId in (:categories)")
-    List<Voucher> filterCoupons(@Param("categories") List<Long>categories,@Param("companies")List<Long>companies);
+    @Query("Select r.sellerId From SellerRating AS r Group by r.sellerId HAVING (AVG(r.stars))>2.0")
+    List<?> ratings();
+
+    @Query("SELECT v from Voucher v where v.id = v.id AND (:companies is null or v.companyId in :companies) AND (:categories is null or v.categoryId in :categories)  AND (:rating is null or v.sellerId in (Select r.sellerId From SellerRating AS r Group by r.sellerId HAVING (AVG(r.stars))>=:rating))")
+    List<Voucher> filterCoupons(@Param("categories") List<Long>categories,@Param("companies")List<Long>companies,@Param("rating") Double averageRating);
 
     List<Voucher> findByVerificationStatus(VoucherVerificationStatus voucherVerificationStatus);
 
