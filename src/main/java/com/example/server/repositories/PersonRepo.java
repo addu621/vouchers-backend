124907a5bd7529de210eb68936138cf6911ef71f
Harshit Garg <harshit.garg@hashedin.com>
2021-06-30 11:54:31 +0530

    moved queries to rating

--- src/main/java/com/example/server/repositories/PersonRepo.java
+++ src/main/java/com/example/server/repositories/PersonRepo.java
@@ -2,6 +2,7 @@ package com.example.server.repositories;
 
 import com.example.server.dto.response.SellerRatingResponse;
 import com.example.server.entities.Person;
+import com.example.server.entities.SellerRating;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,12 +14,5 @@ import java.util.List;
 @Repository
 public interface PersonRepo extends CrudRepository<Person,Long> {
     public Person findByEmail(String email);
-
-    @Query("Select new com.example.server.dto.response.SellerRatingResponse(r.sellerId,AVG(r.stars)) "+"From SellerRating AS r Group by r.sellerId")
-    List<SellerRatingResponse> ratings();
-
-    @Query("Select new com.example.server.dto.response.SellerRatingResponse(r.sellerId,AVG(r.stars)) "+"From SellerRating AS r where r.sellerId = :sellerId Group by r.sellerId")
-    SellerRatingResponse ratingOfSeller(@Param("sellerId") Long sellerId);
-
 }
 
