d32d9ad5726a3b725d06400b1b620f56e48e3880
Harshit Garg <harshit.garg@hashedin.com>
2021-06-30 11:55:53 +0530

    moved service to sellerRatingService

--- src/main/java/com/example/server/services/PersonService.java
+++ src/main/java/com/example/server/services/PersonService.java
@@ -3,6 +3,7 @@ package com.example.server.services;
 import com.example.server.dto.request.PersonRequest;
 import com.example.server.dto.response.SellerRatingResponse;
 import com.example.server.entities.Person;
+import com.example.server.entities.SellerRating;
 import com.example.server.repositories.PersonRepo;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.springframework.beans.BeanUtils.copyProperties;
@@ -54,7 +56,5 @@ public class PersonService {
         return personRepository.findById(id).get();
     }
 
-    public SellerRatingResponse getSellerRating(Long sellerId){
-        return personRepository.ratingOfSeller(sellerId);
-    }
+
 }
