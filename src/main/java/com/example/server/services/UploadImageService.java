2d67677dfc30bd162d5d4421fba49fa33d09fe2c
Harshit Garg <harshit.garg@hashedin.com>
2021-06-30 17:23:36 +0530

    implemented base64 image to cloudinary

--- src/main/java/com/example/server/services/UploadImageService.java
+++ src/main/java/com/example/server/services/UploadImageService.java
@@ -2,6 +2,7 @@ package com.example.server.services;
 ;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,7 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+import org.apache.commons.codec.binary.Base64;
+
+import static com.cloudinary.utils.ObjectUtils.asMap;
 
 @Service
 public class UploadImageService {
@@ -19,21 +24,23 @@ public class UploadImageService {
 
         public String uploadFile(MultipartFile file) {
             try {
-                File uploadedFile = convertMultiPartToFile(file);
-                Map uploadResult = cloudinaryConfig.uploader().upload(uploadedFile, ObjectUtils.emptyMap());
+                byte[] bytesEncoded = Base64.encodeBase64(file.getBytes());
+
+                StringBuilder sb = new StringBuilder();
+                sb.append("data:image/png;base64,");
+                sb.append(Base64.encodeBase64URLSafeString(bytesEncoded));
+                String img = sb.toString();
+
+                Map uploadResult = cloudinaryConfig.uploader().upload(img,ObjectUtils.asMap("resource_type", "auto"));
+
                 return  uploadResult.get("url").toString();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
 
-        private File convertMultiPartToFile(MultipartFile file) throws IOException {
-            File convFile = new File(file.getOriginalFilename());
-            FileOutputStream fos = new FileOutputStream(convFile);
-            fos.write(file.getBytes());
-            fos.close();
-            return convFile;
-        }
+
+
 
     }
 
